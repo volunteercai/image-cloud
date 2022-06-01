9575730c7783dfa735b91e27268181445c459b52
caixinyun <caixinyun>
2022-06-01 13:53:24 +0800

    tj

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use image_cloud::api::image_api;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(||{
+	
+    HttpServer::new(|| {
 		App::new().configure(image_api::config)
 	}).bind(("localhost", 8080))?
 	.run()
